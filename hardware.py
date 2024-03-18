b323e0b2f723b1d5973b6f81b35011fce89f5477
Sheng Tai <alxt9@outlook.com>
2024-03-18 21:44:27 +0200

    defined event and value for hardware.

--- hardware.py
+++ hardware.py
@@ -3,12 +3,35 @@ from ssd1306 import SSD1306_I2C
 import time
 
 
+class Event:
+    EVENT = 1
+    NO_EVENT = 0
+    ERROR = -1
+
+
+class Value:
+    CLOCKWISE = 1
+    COUNTER_CLOCKWISE = -1
+    PRESS = 0
+    RELEASE = 1
+    DEFAULT = 0
+
+
+# all hardware return: status, value
+# status: 0: no event, 1: event, -1: error
+
 class HeartSensor:
     def __init__(self, pin=26):
         self._adc = ADC(Pin(pin))
 
     def read(self):
-        return self._adc.read_u16()
+        """Return (status, value).
+        Status: 1 success; -1: error; value: 0-65535"""
+        try:
+            value = self._adc.read_u16()
+        except:
+            return Event.ERROR, 0  # return immediately if error
+        return Event.EVENT, value
 
 
 class RotaryEncoder:
@@ -17,77 +40,48 @@ class RotaryEncoder:
         self._dt = Pin(dt_pin, Pin.IN, Pin.PULL_UP)
         self._button = Pin(btn_pin, Pin.IN, Pin.PULL_UP)
         self._last_clk_value = 0
-        self._last_button_state = 1
+        self._last_button_state = 1  # ground button, 1 for released
         self._debounce_ms = debounce_ms
         self._last_rotate_time = time.ticks_ms()
         self._last_press_time = time.ticks_ms()
 
-    def read(self):
-        """Return 1 for clockwise, -1 for counter-clockwise, 2 for button press, 0 for no action."""
-        current_time = time.ticks_ms()
-        clk_value = self._clk.value()
-        dt_value = self._dt.value()
-        button_state = self._button.value()
+    def on_rotate(self):
+        """Return (status, value).
+        Status: 0: no event, 1: event, -1: error; value: 1: clockwise, -1: counter-clockwise"""
+        # init default status = 0: no event
+        status = Event.NO_EVENT
+        value = Value.DEFAULT
+        try:
+            clk_value = self._clk.value()
+            dt_value = self._dt.value()
+        except:
+            return Event.ERROR, Value.DEFAULT  # return immediately if error
 
-        # rotate, ignore when button is pressed
-        if clk_value != self._last_clk_value and button_state == 1 and current_time - self._last_rotate_time > self._debounce_ms:
-            if dt_value != clk_value:  # clockwise
-                value = 1
-            else:  # counter-clockwise
-                value = -1
+        current_time = time.ticks_ms()
+        if clk_value != self._last_clk_value and current_time - self._last_rotate_time > self._debounce_ms:
+            status = Event.EVENT
+            value = Value.CLOCKWISE if dt_value != clk_value else Value.COUNTER_CLOCKWISE
             self._last_rotate_time = current_time
             self._last_clk_value = clk_value
-            return value
+        return status, value
 
-        # button
+    def on_press(self):
+        """Return (status, value).
+        Status: 0: no event, 1: event, -1: error; value: 0: press, 1: release"""
+        status = Event.NO_EVENT
+        value = Value.DEFAULT
+        try:
+            button_state = self._button.value()
+        except:
+            return Event.ERROR, Value.DEFAULT
+
+        current_time = time.ticks_ms()
         if button_state != self._last_button_state and current_time - self._last_rotate_time > self._debounce_ms:
-            if button_state == 0:
-                value = 2
-            else:
-                value = 0
+            # ground button, 1 for released, 0 for pressed
+            status = Event.EVENT if button_state == Value.PRESS else Event.NO_EVENT
             self._last_press_time = current_time
             self._last_button_state = button_state
-            return value
-        # always 0 when no action
-        return 0
-
-
-# todo separate the rotary encoder and selector, and move selector maybe? it is not hardware
-class Selector:
-    def __init__(self, clk_pin=10, dt_pin=11, number=1, resolution=5):
-        self.clk = Pin(clk_pin, Pin.IN, Pin.PULL_UP)
-        self.dt = Pin(dt_pin, Pin.IN, Pin.PULL_UP)
-        self.last_encoder_clk_value = 0
-        self.value = 0
-        self.value_max = 0
-        self.number = number
-        self.resolution = resolution
-        # init items count, max value
-        self.set_number(number)
-
-    def read(self):
-        clk_value = self.clk.value()
-        dt_value = self.dt.value()
-        if clk_value != self.last_encoder_clk_value:
-            if dt_value != clk_value and self.value < self.value_max:  # clockwise
-                self.value += 1
-            if dt_value == clk_value and self.value > 0:  # counter-clockwise
-                self.value -= 1
-        self.last_encoder_clk_value = clk_value
-        return int(self.value / self.resolution)
-
-    def read_raw(self):
-        return self.value
-
-    def set_number(self, number):
-        if number > 0:
-            self.number = number
-            self.value_max = number * self.resolution - 1
-            if self.value > self.value_max:
-                self.value = self.value_max
-            return True
-        else:
-            return False
+        return status, value
 
 
 def display_init():

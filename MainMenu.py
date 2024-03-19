9e5c0aa5e3e3af14ac200857d3d1b97028233cff
Sheng Tai <alxt9@outlook.com>
2024-03-19 11:20:04 +0200

    changed function names

--- MainMenu.py
+++ MainMenu.py
@@ -1,6 +1,6 @@
 import time
 from hardware import init_display, RotaryEncoder, Event, Value
-from ui import TextView, OptionView, GraphView
+from ui import TextView, ListView, GraphView
 from common import State
 
 
@@ -10,31 +10,31 @@ class MainMenu:
         self.display = display
         self.rotary_encoder = rotary_encoder
         # init ui
-        self.option_view = OptionView(self.display, ["HR Measure",
-                                                     "HRV Analysis",
-                                                     "Kubios Analysis",
-                                                     "History"])
+        self.list_view = ListView(self.display, ["HR Measure",
+                                                   "HRV Analysis",
+                                                   "Kubios Analysis",
+                                                   "History"], debug=debug)
         self.debug = debug
 
-    def init(self):
+    def enter(self):
         print("MainMenu enter") if self.debug else None
         self.display.fill(0)
-        self.option_view.init()
-        self.option_view.show()
+        self.list_view.refresh()
+        self.list_view.show()
 
     def _select_menu(self):
         event, value = self.rotary_encoder.on_rotate()
         if event == Event.EVENT:
             if value == Value.CLOCKWISE:
-                self.option_view.next()
+                self.list_view.select_next()
             elif value == Value.COUNTER_CLOCKWISE:
-                self.option_view.previous()
+                self.list_view.select_previous()
             # self.option_list.show() # no need when auto show enabled
 
-    def _next(self):
+    def _next_state(self):
         event, value = self.rotary_encoder.on_press()
         if event == Event.EVENT and value == Value.PRESS:
-            selected_index = self.option_view.get_selected_index()
+            selected_index = self.list_view.get_selected_index()
             if selected_index == 0:
                 return State.HR
             elif selected_index == 1:
@@ -51,4 +51,4 @@ class MainMenu:
 
     def execute(self):
         self._select_menu()
-        return self._next()
+        return self._next_state()

6bc9610c18437b0e6a2391e78199edb9a32fe781
Sheng Tai <alxt9@outlook.com>
2024-03-20 00:40:17 +0200

    used irq

--- MainMenu.py
+++ MainMenu.py
@@ -1,37 +1,33 @@
 import time
 from hardware import init_display, RotaryEncoder, EventTypes
-from ui import TextView, ListView, GraphView
+from ui import TextView, ListView, GraphView, MenuView
 from common import State
 
 
 class MainMenu:
-    def __init__(self, display, rotary_encoder, event_manager, debug=False):
+    def __init__(self, display, event_manager, debug=False):
         # init hardware
         self._display = display
-        self._rotary_encoder = rotary_encoder
         self._event_manager = event_manager
         # init ui
-        self._list_view = ListView(self._display, ["HR Measure",
-                                                   "HRV Analysis",
-                                                   "Kubios Analysis",
-                                                   "History"], debug=debug)
+        self._menu_view = MenuView(self._display, debug=debug)
         self._debug = debug
 
     def enter(self):
         print("MainMenu enter") if self._debug else None
         self._display.fill(0)
-        self._list_view.re_init()
-        self._list_view.show()
+        self._menu_view.re_init()
+        self._menu_view.show()
 
     def _select_menu(self):
         if self._event_manager.pop(EventTypes.ENCODER_CLOCKWISE):
-            self._list_view.select_next()
+            self._menu_view.select_next()
         elif self._event_manager.pop(EventTypes.ENCODER_COUNTER_CLOCKWISE):
-            self._list_view.select_previous()
+            self._menu_view.select_previous()
 
     def _next_state(self):
         if self._event_manager.pop(EventTypes.ENCODER_PRESS):
-            selected_index = self._list_view.get_selected_index()
+            selected_index = self._menu_view.get_selected_index()
             if selected_index == 0:
                 return State.HR
             elif selected_index == 1:

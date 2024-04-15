cf94007d1c5eefda9324183dfba6ee01cd6c8e9d
Sheng Tai <alxt9@outlook.com>
2024-04-15 09:47:52 +0300

    renamed packages and class name

--- hardware.py
+++ hardware.py
@@ -1,7 +1,22 @@
 from machine import Pin, I2C, ADC
-from ssd1306 import SSD1306_I2C
+from ssd1306 import SSD1306_I2C as SSD1306_I2C_
 import time
-from lib.fifo import Fifo
+from lib.fifo import Fifo as Fifo_
+
+
+class Fifo(Fifo_):
+    def __init__(self, size, typecode='H', debug=False):
+        super().__init__(size, typecode)
+        self.debug = debug
+
+    def count(self):
+        count = ((self.head - self.tail) + self.size) % self.size
+        return count
+
+    def get(self):
+        if self.debug:
+            print(f"time: {time.ticks_ms()}, fifo count:{self.count()}")
+        return super().get()
 
 
 class ENCODER_EVENT:
@@ -73,17 +88,11 @@ class RotaryEncoder:
             return ENCODER_EVENT.NONE
 
 
-class MySSD1306_I2C(SSD1306_I2C):
+class SSD1306_I2C(SSD1306_I2C_):
     def __init__(self, width, height, i2c):
         self.width = width
         self.height = height
         super().__init__(width, height, i2c)
-
-    def power_off(self):
-        self.poweroff()
-
-    def power_on(self):
-        self.poweron()
 
     def update(self):
         self.show()
@@ -97,41 +106,4 @@ def init_display(sda=14, scl=15, width=128, height=64):
     # https://docs.micropython.org/en/latest/esp8266/tutorial/ssd1306.html
     # https://docs.micropython.org/en/latest/library/framebuf.html
     i2c = I2C(1, scl=Pin(scl), sda=Pin(sda), freq=400000)
-    return MySSD1306_I2C(width, height, i2c)
-
-# class RotaryEncoder:
-#     def __init__(self, clk_pin=10, dt_pin=11, btn_pin=12, debounce_ms=5, rotate_timeout=200, rotate_trigger_count=3):
-#         self._clk = Pin(clk_pin, Pin.IN, Pin.PULL_UP)
-#         self._dt = Pin(dt_pin, Pin.IN, Pin.PULL_UP)
-#         self._button = Pin(btn_pin, Pin.IN, Pin.PULL_UP)
-#         self._last_button_state = 1  # ground button, 1 for released
-#         self._debounce_ms = debounce_ms
-#         self._last_rotate_time = time.ticks_ms()
-#         self._last_press_time = time.ticks_ms()
-#         self._rotate_timeout = rotate_timeout
-#         self._rotate_trigger_count = rotate_trigger_count
-#         self._set_rotate_irq = True
-#         self._fifo = Fifo(10)
-#         # register interrupt
-#         self._clk.irq(trigger=Pin.IRQ_RISING, handler=self._rotate_irq, hard=True)
-#         self._button.irq(trigger=Pin.IRQ_RISING | Pin.IRQ_FALLING, handler=self._press_irq, hard=True)
-#
-#     def _rotate_irq(self, p):
-#         current_time = time.ticks_ms()
-#         if current_time - self._last_rotate_time > self._rotate_timeout:
-#             self._rotate_step = 0
-#
-#         if self._rotate_step >= self._rotate_trigger_count:
-#             self._fifo.put(EventTypes.ENCODER_CLOCKWISE)
-#             self._rotate_step = 0
-#
-#         if self._rotate_step <= -self._rotate_trigger_count:
-#             self._fifo.put(EventTypes.ENCODER_COUNTER_CLOCKWISE)
-#             self._rotate_step = 0
-#
-#         if current_time - self._last_rotate_time > self._debounce_ms:
-#             if self._dt.value():
-#                 self._rotate_step -= 1
-#             else:
-#                 self._rotate_step += 1
-#             self._last_rotate_time = time.ticks_ms()
+    return SSD1306_I2C(width, height, i2c)

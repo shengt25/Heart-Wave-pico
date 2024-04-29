f80e8c7053afaa9629b0a3eb349807f3d5594712
Sheng Tai <alxt9@outlook.com>
2024-04-29 10:56:31 +0300

    Updated sliding window method

--- data_processing.py
+++ data_processing.py
@@ -40,36 +40,40 @@ class IBICalculator:
         self._sensor_fifo = sensor_fifo
         # init ibi array
         self.ibi_fifo = Fifo(20, 'H')
+        self._sampling_rate = sampling_rate
         self._window_side_length = int(sampling_rate * 0.75)
         self._window_size = self._window_side_length * 2 + 1
-        self._count = 0
-        self._sum = 0
-
-        self._max = 0
-        self._peak_index = 0
-        self._last_peak_index = 0
-        self.state = self._state_init
         self._max_ibi = 60 / min_hr * 1000
         self._min_ibi = 60 / max_hr * 1000
+
+        self._debounce_samples = 10
+        self._count = 0
+        self._sum = 0
+        self._max = 0
+        self._last_thr_index = 0
+        self._thr_index = 0
+        self._last_peak_index = 0
+        self._peak_index = 0
+
+        self.state = self._state_wait
 
     def reinit(self):
         self._count = 0
         self._sum = 0
         self._max = 0
-        self._peak_index = 0
+        self._last_thr_index = 0
+        self._thr_index = 0
         self._last_peak_index = 0
-        self.ibi_fifo.clear()
-        self.state = self._state_init
+        self._peak_index = 0
 
-    def _state_init(self):
-        # wait until the first window is filled
+    def _state_wait(self):
         while self._sensor_fifo.has_data():
-            if self._count < self._window_size:
+            if self._thr_index < self._window_size:  # borrow _thr_index to count the number of data points
                 self._sum += self._sensor_fifo.get()
-                self._count += 1
+                self._thr_index += 1
             else:
+                self._thr_index = 0
                 self.state = self._state_below_thr
-                self._count = 0
                 break
 
     def _cal_threshold(self):
@@ -78,50 +82,52 @@ class IBICalculator:
         threshold = int(self._sum / self._window_size)
         threshold = threshold + (threshold >> 6)
         value = self._sensor_fifo.get_previous(self._window_side_length)
-        self._count += 1
+        print(value, threshold)
         return value, threshold
 
     def _state_below_thr(self):
         while self._sensor_fifo.has_data():
             value, threshold = self._cal_threshold()
+            self._thr_index += 1
             if value > threshold:
+                self._last_peak_index = self._peak_index
+                self._last_thr_index = self._thr_index
                 self._max = 0
-                if self._last_peak_index == 0:
-                    self.state = self._state_above_thr_init
-                    break
-                else:
-                    self.state = self._state_above_thr
-                    break
+                self._thr_index = 0
+                self._peak_index = 0
+                # print("above thr")
+                self.state = self._state_above_thr
+                return
 
     def _state_above_thr(self):
         while self._sensor_fifo.has_data():
             value, threshold = self._cal_threshold()
+            self._thr_index += 1
             if value > threshold and value > self._max:
                 self._max = value
-                self._peak_index = self._count
+                self._peak_index = self._thr_index
 
             if value < threshold:
-                ibi = int((self._peak_index - self._last_peak_index) * 1000 / 250)
-                if ibi < 0:
-                    print("ibi is negative", self._peak_index, self._last_peak_index)
-                if self._min_ibi < ibi < self._max_ibi:
-                    self.ibi_fifo.put(ibi)
-                    self._last_peak_index = self._peak_index
+
+                # if self._max == 0:
+                #     print("max is 0")
+                # if self._peak_index == 0:
+                #     print("peak index is 0")
+
+                if self._last_peak_index == 0:
+                    # print("below thr, not calculating")
+                    self.state = self._state_below_thr
+                    return
                 else:
-                    self._last_peak_index = 0
+                    data_points = self._last_thr_index - self._last_peak_index + self._peak_index
+                    ibi = int(data_points * 1000 / self._sampling_rate)
+                    if self._min_ibi < ibi < self._max_ibi:
+                        self.ibi_fifo.put(ibi)
+                    else:
+                        self._peak_index = 0
+                # print("below thr")
                 self.state = self._state_below_thr
-                break
-
-    def _state_above_thr_init(self):
-        while self._sensor_fifo.has_data():
-            value, threshold = self._cal_threshold()
-            if value > threshold and value > self._max:
-                self._max = value
-                self._last_peak_index = self._count
-
-            if value < threshold:
-                self.state = self._state_below_thr
-                break
+                return
 
     def run(self):
         self.state()

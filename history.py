3dfa79f510df8f6df7ae782a9a8dcfd0ba745fd5
Sheng Tai <alxt9@outlook.com>
2024-04-30 01:21:32 +0300

    Fixed: position was reset when exiting from data to list

--- history.py
+++ history.py
@@ -23,6 +23,12 @@ class History:
         self._page = 0
         self._state_machine = state_machine
 
+    def enter(self):
+        print_log("History: enter")
+        self._selection = 0  # reset selected index
+        self._page = 0  # reset view index start
+        self._state_machine.set(self._state_show_list_init)
+
     def _load_history_list(self):
         # dummy data
         self._history_list = ["back", "2021-01-01", "2021-01-02", "2021-01-03", "2021-01-04", "2021-01-05",
@@ -32,13 +38,11 @@ class History:
         # dummy data
         self._data = {"date": "2021-01-01", "hr": "60", "hrv": "10", "sdnn": "20", "rmssd": "30"}
 
-    def enter(self):
-        print_log("History: enter")
+    def _state_show_list_init(self):
+        # load data
         self._load_history_list()
-        self._state_machine.set(self._show_history_init)
 
-    def _show_history_init(self):
-        # ui
+        # create ui elements
         self._textview_history_heading = self._view.add_text(text="History", y=0, invert=True)
         self._listview_history_list = self._view.add_list(items=self._history_list, y=14)
         self._listview_history_list.set_page(self._page)
@@ -47,9 +51,9 @@ class History:
         # other
         self._rotary_encoder.set_rotate_irq(items_count=len(self._history_list), position=self._selection,
                                             loop_mode=False)
-        self._state_machine.set(self._show_history)
+        self._state_machine.set(self._state_show_list)
 
-    def _show_history(self):
+    def _state_show_list(self):
         event = self._rotary_encoder.get_event()
         if event == EncoderEvent.ROTATE:
             self._selection = self._rotary_encoder.get_position()
@@ -59,18 +63,18 @@ class History:
             self._page = self._listview_history_list.get_page()  # save current page for switching back
             if self._selection == 0:
                 self._rotary_encoder.unset_rotate_irq()
-                self._view.deactivate_all()
+                self._view.remove_all()
                 self._state_machine.set(self._state_machine.main_menu.enter)
             else:
                 self._rotary_encoder.unset_rotate_irq()
-                self._view.deactivate_all()
-                self._state_machine.set(self._show_data_init)
+                self._view.remove_all()
+                self._state_machine.set(self._state_show_data_init)
 
-    def _show_data_init(self):
-        print_log("History: history data")
+    def _state_show_data_init(self):
+        # load data
         self._load_data()
 
-        # ui
+        # create ui elements
         self._textview_data_heading = self._view.add_text(text=self._data["date"], y=0, invert=True)
         self._textview_data_hr = self._view.add_text(text="HR: " + self._data["hr"], y=12)
         self._textview_data_hrv = self._view.add_text(text="HRV: " + self._data["hrv"], y=22)
@@ -78,19 +82,10 @@ class History:
         self._textview_data_rmssd = self._view.add_text(text="RMSSD: " + self._data["rmssd"], y=42)
 
         # other
-        self._state_machine.set(self._show_data)
+        self._state_machine.set(self._state_show_data)
 
-    def _show_data(self):
+    def _state_show_data(self):
         event = self._rotary_encoder.get_event()
         if event == EncoderEvent.PRESS:
-            self._view.deactivate_all()
-            self._state_machine.set(self._show_history_init)
-
-    def _exit(self):
-        # clean up resources and unset irq
-        self._view.deactivate_all()
-        self._rotary_encoder.unset_rotate_irq()
-        self._display.clear()
-        self._selection = 0  # reset selected index
-        self._page = 0  # reset view index start
-        self._state_machine.set(self._state_machine.main_menu.enter)
+            self._view.remove_all()
+            self._state_machine.set(self._state_show_list_init)

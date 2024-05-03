e2b19e1c99e604d6d870ffc807243bc9aa4ae96f
Sheng Tai <alxt9@outlook.com>
2024-05-04 02:33:48 +0300

    refactored code, reused states such as HREntry, etc. moved module tracker from state class to statemachine

--- hrv.py
+++ hrv.py
@@ -1,23 +1,9 @@
-from hardware import EncoderEvent
-from utils import print_log, pico_rom_stat
 import time
-from data_processing import HRVCalculator
+from utils import print_log, pico_rom_stat
 from save_system import save_system
 import machine
 from state import State
-from hr import HREntry
-
-
-class HRVEntry(HREntry):
-    def __init__(self, state_machine):
-        super().__init__(state_machine)
-        self._hr_update_interval = 5  # number of sample needed to update the HR display
-        self._start_threshold = 200  # threshold that triggers measurement automatically when finger is placed
-        self._heading_text = "HRV Analysis"
-        self._hr_text = "-- BPM  30s"
-
-    def exit(self):
-        self._state_machine.set(HRVMeasure)
+from data_processing import calculate_hrv
 
 
 class HRVMeasure(State):
@@ -25,143 +11,138 @@ class HRVMeasure(State):
         super().__init__(state_machine)
         # data
         self._last_graph_update_time = time.ticks_ms()
-        self._last_countdown_update_time = 0
+        self._last_countdown_update_time = -1
         self._hr = 0
         self._hr_display_list = []
         self._ibi_list = []
         self._time_left = 30
         self._ibi_fifo = self._ibi_calculator.get_ibi_fifo()
         # ui placeholder
-        self._text_hr = None
-        self._graph = None
+        self._textview_hr = None
+        self._graphview = None
         # settings
-        self._hr_update_interval = 5
+        self._hr_update_interval = 5  # number of sample
 
-    def enter(self):
-        self._ibi_calculator.reinit()  # ibi_fifo will be cleared inside this method
+    def enter(self, args):
+        # re-init data
+        self._last_graph_update_time = time.ticks_ms()
+        self._last_countdown_update_time = -1
         self._hr = 0
         self._hr_display_list = []
-        self._time_left = 30
         self._ibi_list = []
-        self._last_graph_update_time = time.ticks_ms()
-        self._last_countdown_update_time = 0
-        # set up timer interrupt at last (to reduce the chance of data building up), and set next state to measure
-        self._graph = self._view.add_graph(y=14, h=64 - 14 - 12)
-        self._text_hr = self._view.select_by_id("hr")
+        self._time_left = 30
+        self._ibi_calculator.reinit()
+        # ui elements
+        self._graphview = self._view.add_graph(y=14, h=64 - 14 - 12)
+        self._textview_hr = self._view.select_by_id("hr")
         self._heart_sensor.start()
 
     def loop(self):
-        # this function was assigned to the state machine, and called repeatedly in a loop
-        # until entering another state
-
         self._ibi_calculator.run()  # keep calling calculator
         # monitor and get data from ibi fifo, calculate hr and put into list
         if self._ibi_fifo.has_data():
             ibi = self._ibi_fifo.get()
             self._hr_display_list.append(int(60000 / ibi))
             self._ibi_list.append(ibi)
-            # if not started counting down, start it when the first ibi is received
+            # if counting down not started, start it when the first ibi is received
             if self._last_countdown_update_time == 0:
                 self._last_countdown_update_time = time.ticks_ms()
 
-        # for every _hr_update_interval samples, calculate the median value and update the HR display
+        # update hr every 5 samples
         if len(self._hr_display_list) >= self._hr_update_interval:
             self._hr = sorted(self._hr_display_list)[len(self._hr_display_list) // 2]
             # use set_text method to update the text, view (screen) will auto refresh
             if self._hr == 0:
-                self._text_hr.set_text("-- BPM  " + str(self._time_left) + "s")
+                self._textview_hr.set_text("-- BPM  " + str(self._time_left) + "s")
             else:
-                self._text_hr.set_text(str(self._hr) + " BPM  " + str(self._time_left) + "s")
+                self._textview_hr.set_text(str(self._hr) + " BPM  " + str(self._time_left) + "s")
             self._hr_display_list.clear()
 
-        # update graph, it reads current value from sensor directly. no need to use sensor_fifo
-        # only update screen every 40ms, to save the CPU time
+        # update graph
         if time.ticks_diff(time.ticks_ms(), self._last_graph_update_time) > 40:
             self._last_graph_update_time = time.ticks_ms()
             value = self._heart_sensor.read()
-            self._graph.set_value(value)
+            self._graphview.set_value(value)
 
-        # after the timer is started, update and check 30s timer every second
-        if (self._last_countdown_update_time != 0 and
+        # counting down and update hr text(timer started when the first ibi is received)
+        if (self._last_countdown_update_time != 0 and self._time_left > 0 and
                 time.ticks_diff(time.ticks_ms(), self._last_countdown_update_time) >= 1000):
             self._time_left -= 1
             self._last_countdown_update_time = time.ticks_ms()
-
-            # update or exit when time is up
-            if self._time_left > 0:
-                if self._hr == 0:
-                    self._text_hr.set_text("-- BPM  " + str(self._time_left) + "s")
-                else:
-                    self._text_hr.set_text(str(self._hr) + " BPM  " + str(self._time_left) + "s")
+            if self._hr == 0:
+                self._textview_hr.set_text("-- BPM  " + str(self._time_left) + "s")
             else:
-                # finished measuring, remove all ui elements except for heading, stop heart sensor and go to next state
-                self._view.remove(self._graph)
-                self._view.remove(self._text_hr)
-                self._heart_sensor.stop()
-                self.exit()  # entry point to next state
-                return
+                self._textview_hr.set_text(str(self._hr) + " BPM  " + str(self._time_left) + "s")
 
-        # keep watching rotary encoder press event, if pressed during measuring, exit to main menu
-        event = self._rotary_encoder.get_event()
-        if event == EncoderEvent.PRESS:
-            # remove all ui elements, stop heart sensor, and exit to main menu
-            self._view.remove_all()
+        # exit when time's up
+        if self._time_left <= 0:
             self._heart_sensor.stop()
-            self._state_machine.set(State.Main_Menu)
+            self._view.remove(self._graphview)
+            self._view.remove(self._textview_hr)
+            self._state_machine.set(state_code=self._state_machine.STATE_HRV_RESULT_CHECK, args=[self._ibi_list])
+            return
 
-    def exit(self):
-        """This state creates an entry point to next state,
-        making kubios module able to easily inherit HRV class and override this method."""
-        self._state_machine.set(HRVResultCheck, self._ibi_list)
+        # rotary encoder event: press
+        event = self._rotary_encoder.get_event()
+        if event == self._rotary_encoder.EVENT_PRESS:
+            self._heart_sensor.stop()
+            self._view.remove_all()
+            self._state_machine.set(self._state_machine.STATE_MENU)
+            return
 
 
 class HRVResultCheck(State):
-    def __init__(self, state_machine, ibi_list):
+    def __init__(self, state_machine):
         super().__init__(state_machine)
-        self._ibi_list = ibi_list
         self._listview_retry = None
         self._selection = 0
 
-    def enter(self):
+    def enter(self, args):
+        ibi_list = args[0]
         self._selection = 0
-        if len(self._ibi_list) < 10:
+        # data not enough, retry or exit
+        if len(ibi_list) < 10:
             self._view.add_text(text="Not enough data", y=14, vid="info1")
             self._listview_retry = self._view.add_list(items=["Try again", "Exit"], y=34)
-            self._rotary_encoder.set_rotate_irq(items_count=2, position=0, loop_mode=False)
+            self._rotary_encoder.set_rotate_irq(items_count=2, position=0)
         else:
-            self._state_machine.set(HRVResult, self._ibi_list)
+            # data ok, show result, or go to kubios
+            if self._state_machine.current_module == self._state_machine.MODULE_HRV:
+                self._state_machine.set(state_code=self._state_machine.STATE_HRV_RESULT_SHOW, args=[ibi_list])
+            elif self._state_machine.current_module == self._state_machine.MODULE_KUBIOS:
+                self._state_machine.set(state_code=self._state_machine.STATE_KUBIOS_SEND, args=[ibi_list])
 
     def loop(self):
+        """if check not enough, then goes here"""
         event = self._rotary_encoder.get_event()
-        if event == EncoderEvent.ROTATE:
+        if event == self._rotary_encoder.EVENT_ROTATE:
             self._selection = self._rotary_encoder.get_position()
             self._listview_retry.set_selection(self._selection)
-        elif event == EncoderEvent.PRESS:
+        elif event == self._rotary_encoder.EVENT_PRESS:
+            self._rotary_encoder.unset_rotate_irq()
+            self._view.remove_all()
             if self._selection == 0:
-                self._rotary_encoder.unset_rotate_irq()
-                self._view.remove_all()
-                self._state_machine.set(HRVEntry)
+                self._state_machine.set(state_code=self._state_machine.STATE_HR_ENTRY)
+            elif self._selection == 1:
+
+                self._state_machine.set(state_code=self._state_machine.STATE_MENU)
             else:
-                self._rotary_encoder.unset_rotate_irq()
-                self._view.remove_all()
-                self._state_machine.set(State.Main_Menu)
+                raise ValueError("Invalid selection index")
 
 
-class HRVResult(State):
-    def __init__(self, state_machine, ibi_list):
+class HRVResultShow(State):
+    def __init__(self, state_machine):
         super().__init__(state_machine)
-        self._ibi_list = ibi_list
 
-    def enter(self):
+    def enter(self, args):
         # calculating result
-        HRV_Calculator = HRVCalculator(self._ibi_list)
-        hr, ppi, rmssd, sdnn = HRV_Calculator._calculate_results()
+        ibi_list = args[0]
+        hr, ppi, rmssd, sdnn = calculate_hrv(ibi_list)
 
         # create new list elements
-        self._view.add_list(items=["HR: " + str(hr) + " BPM",
-                                   "PPI: " + str(ppi) + " ms",
-                                   "RMSSD: " + str(rmssd) + " ms",
-                                   "SDNN: " + str(sdnn) + " ms"], y=14, read_only=True)
+        self._view.add_list(items=["HR: " + str(hr) + " BPM", "PPI: " + str(ppi) + " ms",
+                                   "RMSSD: " + str(rmssd) + " ms", "SDNN: " + str(sdnn) + " ms"],
+                            y=14, read_only=True)
 
         # time initialization
         rtc = machine.RTC()
@@ -181,7 +162,7 @@ class HRVResult(State):
     def loop(self):
         # keep watching rotary encoder press event
         event = self._rotary_encoder.get_event()
-        if event == EncoderEvent.PRESS:
+        if event == self._rotary_encoder.EVENT_PRESS:
             # remove all ui elements and exit to main menu
             self._view.remove_all()
-            self._state_machine.set(State.Main_Menu)
+            self._state_machine.set(self._state_machine.STATE_MENU)

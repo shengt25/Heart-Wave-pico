70a3ee9dbd7e36f9e19a390bab975b8b1e13f165
Sheng Tai <alxt9@outlook.com>
2024-05-04 02:35:25 +0300

    used new STATE_CODE. added arguments pass to state. set MODULE_CODE to statemachine.

--- main_menu.py
+++ main_menu.py
@@ -1,38 +1,42 @@
 import time
-from hardware import EncoderEvent
 from state import State
 from utils import print_log
 
 
 class MainMenu(State):
     def __init__(self, state_machine):
+        # take common resources:
+        # state_machine, rotary_encoder, heart_sensor, ibi_calculator, view
         super().__init__(state_machine)
-        # init
         # ui placeholder
         self._menu = None
-        # other
-        self._selection = 0  # for preserve selected index
+        # data
+        self._selection = 0  # to preserve selected index, resume when exit and re-enter
 
-    def enter(self):
-        print_log("MainMenu enter")
+    def enter(self, args):
         self._rotary_encoder.set_rotate_irq(items_count=4, position=self._selection, loop_mode=False)
         self._menu = self._view.add_menu()
         self._menu.set_selection(self._selection)  # resume selected index from last time
 
     def loop(self):
         event = self._rotary_encoder.get_event()
-        if event == EncoderEvent.ROTATE:
+        if event == self._rotary_encoder.EVENT_ROTATE:
             self._selection = self._rotary_encoder.get_position()
             self._menu.set_selection(self._selection)
-        elif event == EncoderEvent.PRESS:
+        elif event == self._rotary_encoder.EVENT_PRESS:
             if self._selection == 0:
-                self._state_machine.set(State.HR_Entry)
+                # args: [heading text, hr text]
+                self._state_machine.set_module(self._state_machine.MODULE_HR)
+                self._state_machine.set(self._state_machine.STATE_HR_ENTRY, ["HR Measure", "-- BPM"])
             elif self._selection == 1:
-                self._state_machine.set(State.HRV_Entry)
+                self._state_machine.set_module(self._state_machine.MODULE_HRV)
+                self._state_machine.set(self._state_machine.STATE_HR_ENTRY, ["HRV Analysis", "-- BPM  30s"])
             elif self._selection == 2:
-                self._state_machine.set(State.Kubios_Entry)
+                self._state_machine.set_module(self._state_machine.MODULE_KUBIOS)
+                self._state_machine.set(self._state_machine.STATE_HR_ENTRY, ["HRV Analysis", "-- BPM  30s"])
             elif self._selection == 3:
-                self._state_machine.set(State.History_Entry)
+                self._state_machine.set_module(self._state_machine.MODULE_HISTORY)
+                self._state_machine.set("later haha")
             else:
                 raise ValueError("Invalid selection index")
             self._rotary_encoder.unset_rotate_irq()

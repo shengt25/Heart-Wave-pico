30ad326bcf42af4f1510ccd06cedbb65a06211d8
Sheng Tai <alxt9@outlook.com>
2024-05-04 16:14:31 +0300

    moved load history function from other module to here

--- save_system.py
+++ save_system.py
@@ -33,8 +33,25 @@ def save_system(data):
     except OSError:
         os.mkdir(directory)
     date = data["DATE"]
-    # for year only save the last two digits, because screen is too small to display 4
+    # format: DD.MM.YY hh:mm:ss,
+    # only last two digits for year, because screen is too small to display 4
+    # but seconds are important to distinguish files saved in the same minute, just in case
+    # seconds will be cut off in listview, also because of the small screen
     file_name = directory + "/" + date[:5] + "." + date[8:] + ".txt"
     with open(file_name, "w") as file:
         json.dump(data, file)
     return True
+
+
+def load_history_list():
+    directory = GlobalSettings.save_directory + "/"
+    files = os.listdir(directory)
+    return files
+
+
+def load_history_data(file_name):
+    directory = GlobalSettings.save_directory + "/"
+    path = directory + file_name
+    with open(path, 'r') as file:
+        json_data = json.load(file)
+    return json_data

c9e0046b86b8688cfc36875acd91d6f3b79e625c
Sheng Tai <alxt9@outlook.com>
2024-05-05 03:26:03 +0300

    added loading settings function, added utility function to get time and convert dict(json) to show items in listview

--- utils.py
+++ utils.py
@@ -2,12 +2,19 @@ import time
 import os
 import gc
 import machine
+import json
 
 
 class GlobalSettings:
     print_log = False
-    files_limit = 10
     save_directory = "Saved_Values"
+    files_limit = 1000
+    wifi_ssid = ""
+    wifi_password = ""
+    mqtt_broker_ip = ""
+    kubios_apikey = ""
+    kubios_client_id = ""
+    kubios_client_secret = ""
 
 
 def print_log(message):
@@ -34,3 +41,46 @@ def pico_rom_stat():
     s = os.statvfs('/')
     storage_free = s[0] * s[3] / 1024
     return storage_free
+
+
+def load_settings(filename):
+    """Parse settings from settings.json file, saved as json format."""
+    try:
+        with open(filename, "r") as file:
+            settings = json.load(file)
+            GlobalSettings.files_limit = settings["files_limit"]
+            GlobalSettings.wifi_ssid = settings["wifi_ssid"]
+            GlobalSettings.wifi_password = settings["wifi_password"]
+            GlobalSettings.mqtt_broker_ip = settings["mqtt_broker_ip"]
+            GlobalSettings.kubios_apikey = settings["kubios_apikey"]
+            GlobalSettings.kubios_client_id = settings["kubios_client_id"]
+            GlobalSettings.kubios_client_secret = settings["kubios_client_secret"]
+    except OSError:
+        print_log("Settings file not found")
+
+
+def get_datetime():
+    rtc = machine.RTC()  # time initialization
+    year, month, day, _, hour, minute, second, _ = rtc.datetime()
+    year = year % 100  # only last 2 digits
+    datetime = "{:02d}.{:02d}.{} {:02d}:{:02d}:{:02d}".format(day, month, year, hour, minute, second)
+    return datetime
+
+
+def dict2show_items(dict_data, show_datetime=False):
+    list_data = []
+    # history data: date time first
+    if show_datetime:
+        list_data = ["Date:" + str(dict_data["DATE"][:8]),
+                     "Time:" + str(dict_data["DATE"][9:17])]
+    # normal data: in the middle
+    list_data.extend(["HR:" + str(dict_data["HR"]),
+                      "PPI:" + str(dict_data["PPI"]),
+                      "RMSSD:" + str(dict_data["RMSSD"]),
+                      "SDNN:" + str(dict_data["SDNN"])])
+    # kubios data: at the end
+    if "SNS" in dict_data:
+        list_data.extend(["SNS:" + str(dict_data["SNS"]),
+                          "PNS:" + str(dict_data["PNS"]),
+                          "Stress:" + str(dict_data["STRESS"])])
+    return list_data
